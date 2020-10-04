48786a6583efb80a3d496bbfa4da0c7798f1f437
Michał Stanik <micha.stanik@gmail.com>
2020-10-04 20:23:04 +0200

    AirportList - change for ICAO load

--- MST.FCT/MST.FCT.App.Server/Pages/Airports/AirportsList.razor.cs
+++ MST.FCT/MST.FCT.App.Server/Pages/Airports/AirportsList.razor.cs
@@ -4,11 +4,14 @@ using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
 
 namespace MST.FCT.App.Server.Pages.Airports
 {
     public partial class AirportsList : ComponentBase
     {
+        private bool includedICCO;
+
         [Inject]
         public IAirportDataService AirportDataService { get; set; }
 
@@ -17,8 +20,19 @@ namespace MST.FCT.App.Server.Pages.Airports
 
         public AirportModel SelectedAirport { get; set; }
 
+        public bool IncludedICCO 
+        {
+            get => includedICCO;
+            set
+            {
+                includedICCO = value;
+                //IncludeIcao(value);
+            }
+        }
+
         protected override async Task OnInitializedAsync()
         {
+            IncludedICCO = false;
             Airports = (await AirportDataService.GetAllAirports()).ToList();
             Airports = Airports.Where(c => !string.IsNullOrWhiteSpace(c.IATA)).ToList();
         }
@@ -27,9 +41,18 @@ namespace MST.FCT.App.Server.Pages.Airports
             SelectedAirport = airport;
         }
 
-        private void IncludeIcao()
+        private async void IncludeIcao(bool includeICCAO)
         {
-
+            if (includeICCAO)
+            {
+                Airports = null;
+                Airports = (await AirportDataService.GetAllAirports()).ToList();
+            }
+            else
+            {
+                Airports = (await AirportDataService.GetAllAirports()).ToList();
+                Airports = Airports.Where(c => !string.IsNullOrWhiteSpace(c.IATA)).ToList();
+            }
         }
     }
 }
